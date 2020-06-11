4d0bd0d44dfb7a2f0ed5afaf4e1b0dac3bbeccb6
Luc Urlings <urlingsluc@gmail.com>
2020-06-11 19:02:50 +0200

    feature/KL-95: Task/KL-100: added sentry

--- src/_components/NavBar/NavBar.jsx
+++ src/_components/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
 import {Link} from 'react-router-dom';
-import * as textcomplete from 'textcomplete'
 import './NavBar.css'
 
 class NavBar extends Component {
